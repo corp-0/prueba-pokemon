d51468e9f38210f6953e58c17d214c8b6836e632
Andrés Riquelme <andresfranco.rs@gmail.com>
2022-01-13 21:15:52 -0300

    feat: add utils folder

---
+++ app/utils/unit_utils.rb
@@ -0,0 +1,9 @@
+class UnitUtils
+  def self.hectogram_to_kg(hectograms)
+    hectograms * 0.1
+  end
+
+  def self.decimeter_to_meter(decimeters)
+    decimeters * 0.1
+  end
+end
--- config/application.rb
+++ config/application.rb
@@ -5,11 +5,11 @@ require "rails/all"
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
-
 module PruebaPokemon
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
+    config.autoload_paths += %W(#{config.root}/app/utils)
 
     # Configuration for the application, engines, and railties goes here.
     #
