26510d2afe3c08b6796dda299e60d93177c74cb1
Andrés Riquelme <andresfranco.rs@gmail.com>
2022-01-14 15:11:17 -0300

    feat: improve unit utils to return humanised results

--- app/utils/unit_utils.rb
+++ app/utils/unit_utils.rb
@@ -1,9 +1,35 @@
 class UnitUtils
   def self.hectogram_to_kg(hectograms)
-    hectograms * 0.1
+    unit = 'kilogramo'
+    kg = (hectograms * 0.1).ceil(2)
+    if kg < 1
+      unit = 'gramo'
+      g = (kg * 1000).ceil(2)
+      humanize_unit(g, unit)
+    end
+    humanize_unit(kg, unit)
   end
 
   def self.decimeter_to_meter(decimeters)
-    decimeters * 0.1
+    unit = 'metro'
+    m = (decimeters * 0.1).ceil(2)
+    if m < 1
+      unit = 'centímetro'
+      cm = (m * 100).ceil(2)
+      return humanize_unit(cm, unit)
+    end
+    humanize_unit(m, unit)
+  end
+
+  def self.humanize_unit(value, unit)
+    if value == 1
+      "#{try_integer(value)} #{unit}"
+    else
+      "#{try_integer(value)} #{unit}s"
+    end
+  end
+
+  def self.try_integer(float_number)
+    float_number.to_i == float_number ? float_number.to_i : float_number
   end
 end
